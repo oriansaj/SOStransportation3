8d4f20ed6675d004f7163744846097529c4f99bb
oriansaj <oriansaj@rose-hulman.edu>
2020-07-02 11:02:09 -0400

    Made main and selection UIs, and login takes you to selection screen

--- App/app/src/main/java/com/example/indygoreservation/MainActivity.java
+++ App/app/src/main/java/com/example/indygoreservation/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.indygoreservation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void login(View view) {
+        Intent intent = new Intent(this, SelectionScreen.class);
+        startActivity(intent);
     }
 }
---
+++ App/app/src/main/java/com/example/indygoreservation/SelectionScreen.java
@@ -0,0 +1,14 @@
+package com.example.indygoreservation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectionScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selection_screen);
+    }
+}
