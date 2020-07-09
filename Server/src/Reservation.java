23731aa255981ee3a226d58486bd7d0151e24e17
oriansaj <oriansaj@rose-hulman.edu>
2020-07-09 16:24:11 -0400

    Added a system to check capacity and number of reservations (i.e. if the requested one overlaps with current ones, how many, and if it would go over capacity)

--- Server/src/Reservation.java
+++ Server/src/Reservation.java
@@ -5,6 +5,8 @@ import java.util.PriorityQueue;
 
 public class Reservation implements Comparable<Reservation> {
 
+	public static final int MAX_CAPACITY = 5; // arbitrary number
+
 	private static HashMap<String, PriorityQueue<Reservation>> reservations = new HashMap<String, PriorityQueue<Reservation>>();
 
 	private String firstname;
@@ -25,15 +27,7 @@ public class Reservation implements Comparable<Reservation> {
 
 		System.out.println("Name: " + firstname + " " + lastname + "\nEmail: " + email + "\nStart: "
 				+ this.startTime.toString() + "\nEnd: " + this.endTime.toString() + "\nRoute: " + route);
-
-		if (!reservations.containsKey(route)) {
-			PriorityQueue<Reservation> temp = new PriorityQueue<Reservation>();
-			temp.add(this);
-			reservations.put(route, temp);
-		} else {
-			reservations.get(route).add(this);
-			// System.out.println(reservations.get(route).toString());
-		}
+		System.out.println(add(this));
 	}
 
 	@Override
@@ -45,8 +39,67 @@ public class Reservation implements Comparable<Reservation> {
 		return this.startTime;
 	}
 
+	public LocalTime getEndTime() {
+		return this.endTime;
+	}
+
 	@Override
 	public String toString() {
 		return this.startTime.toString();
+	}
+
+	/**
+	 * Checks if two reservations overlap (i.e. start or end time falls within the
+	 * other, or if start/end time equals the other start/end time)
+	 * 
+	 * @param other reservation to check against
+	 * @return if the two overlap
+	 */
+	public boolean overlaps(Reservation other) {
+		return this.startTime.equals(other.startTime) || this.endTime.equals(other.endTime)
+				|| (this.startTime.isAfter(other.getStartTime()) && this.startTime.isBefore(other.getEndTime()))
+				|| (this.endTime.isAfter(other.getStartTime()) && this.endTime.isBefore(other.getEndTime()));
+	}
+
+	/**
+	 * Adds a reservation to the list if there is room
+	 * 
+	 * @param reservation to add
+	 * @return whether it could be added
+	 */
+	private static boolean add(Reservation toAdd) {
+		if (!reservations.containsKey(toAdd.route)) {
+			// There are no other reservations, so capacity is no issue. Just add it
+			PriorityQueue<Reservation> temp = new PriorityQueue<Reservation>();
+			temp.add(toAdd);
+			reservations.put(toAdd.route, temp);
+			return true;
+		} else {
+			// Check is there is enough room. Add and return true is there is, return false
+			// and do nothing otherwise
+			PriorityQueue<Reservation> line = reservations.get(toAdd.route);
+			if (line.size() < MAX_CAPACITY) {
+				// There are fewer reservations than the allowed maximum at a time. Capacity is
+				// no issue, so add it
+				reservations.get(toAdd.route).add(toAdd);
+				return true;
+			} else {
+				// Check how many reservations will be in use at the requested time. If it's
+				// more than the limit, deny it, otherwise add
+				int overlaps = 0;
+				for (Reservation r : line) {
+					if (toAdd.overlaps(r)) {
+						overlaps++;
+					}
+				}
+				if (overlaps < MAX_CAPACITY) {
+					reservations.get(toAdd.route).add(toAdd);
+					return true;
+				} else {
+					return false;
+				}
+			}
+			// System.out.println(reservations.get(route).toString());
+		}
 	}
 }
